c5be2bce4b7c776bb6d6373b767e771d001c9961
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-10-14 14:20:05 -0300

    adicionando elementos as camadas

---
+++ Model/Model.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MVC_LOGIN\Model;
+
+abstract class Model {
+
+    /**
+     * Propriedade que armazenará o array retornado da DAO com a listagem das pessoas.
+     */
+    public $rows;
+
+}
